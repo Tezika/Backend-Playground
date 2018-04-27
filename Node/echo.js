5bae4c5ba576889142e97e1f85f4cd2dab09ef6e
Tezika Zhou <tezikazhou@gmail.com>
2018-04-27 08:52:31 +0000

    add the feeds of node

---
+++ Node/echo.js
@@ -0,0 +1,11 @@
+
+function echo(content, times){
+    for (var i = 0; i < times; i++) {
+        console.log(content);
+    }
+}
+
+echo("Echo!!!", 3);
+echo("Tater toes", 10);
+echo("Hello node", 5);
+echo("Hello Utah EAE",6);
