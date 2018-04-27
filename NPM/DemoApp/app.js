ad9c3760a9554725e72d0f0adf52c94c4c517374
Tezika Zhou <tezikazhou@gmail.com>
2018-04-27 10:03:17 +0000

    update the app.js

--- NPM/DemoApp/app.js
+++ NPM/DemoApp/app.js
@@ -1,5 +1,9 @@
+//cat-me
 var cat = require("cat-me");
 console.log(cat());
+
 console.log("========== I'm a divided line ========== ");
+
+//knock-knock-jokes
 var joke = require("knock-knock-jokes");
 console.log(joke());
