d46fc245c6620e3b1193284a6c989a330b28966a
Tezika Zhou <tezikazhou@gmail.com>
2018-04-28 04:12:48 +0000

    add some route parameters' practices

--- Express/FirstExpressApp/app.js
+++ Express/FirstExpressApp/app.js
@@ -15,6 +15,18 @@ app.get("/cat", function(req, res) {
     res.send("MEOW!!");
 });
 
+app.get("/r/:subreddit", function(req, res){
+    res.send("Welcome to the subreddit, " + req.params.subreddit + ":)"); 
+});
+
+app.get("/r/:subreddit/comments/:id/:title", function(req, res){
+    res.send("Welcome to the comment page."); 
+});
+
+app.get("*", function(req, res){
+    res.send("Wow, you're a star.");
+});
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The server has started......");
 });
