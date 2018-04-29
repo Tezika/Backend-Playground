fa39d15b43c9e97032ea117c4fcaef668bc10604
Tezika Zhou <tezikazhou@gmail.com>
2018-04-29 02:55:04 +0000

    add the post route :)

--- Express/PostRequestDemo/app.js
+++ Express/PostRequestDemo/app.js
@@ -1,12 +1,25 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var friends = ["Tezika", "Tarsh", "Sukun", "Jam", "Violet"];
 
 app.get("/", function(req, res) {
     res.render("home");
+});
+
+app.get("/friends", function(req, res) {
+    res.render("friends", { friends: friends });
+});
+
+app.post("/addfriend", function(req, res) {
+    var newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
