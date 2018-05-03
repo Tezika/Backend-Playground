ef2dd7f62afebd1caa14d273640dc4567339f02a
Tezika Zhou <tezikazhou@gmail.com>
2018-05-03 04:49:25 +0000

    integrate the monogo into the yelp camp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,9 +1,19 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine ", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 var campgrounds = [
     { name: "Salmon Creek", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b0144390f2c57dafeab6_340.jpg" },
@@ -17,20 +27,31 @@ var campgrounds = [
     { name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg" }
 ];
 
+
 app.get("/", function(req, res) {
     res.render("landing.ejs");
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds.ejs", { campgrounds: campgrounds })
+    Campground.find({}, function(err, campgrounds) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Retrieve all campgrounds from the database successfully.");
+            res.render("campgrounds.ejs", { campgrounds: campgrounds })
+        }
+    });
+
 });
 
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var img = req.body.img;
-    var newCampground = { name: name, image: img };
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    createNewCampground(name, img,function(){
+        res.redirect("/campgrounds");
+    });
+
 });
 
 app.get("/campgrounds/new", function(req, res) {
@@ -41,3 +62,26 @@ app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Open the YelpCamp server successfully!");
 });
 
+function createNewCampground(name, image, callback) {
+    Campground.create({
+        name: name,
+        image: image
+    }, function(err, campground) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Create new campground in the database successfully");
+            console.log(campground);
+        }
+        if (callback != null) {
+            callback();
+        }
+    });
+}
+
+function addTemporaryCampgrounds() {
+    campgrounds.forEach(function(cg) {
+        createNewCampground(cg.name, cg.image);
+    });
+}
