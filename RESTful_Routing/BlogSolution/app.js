99700b7cf8e4ceafe1dfb9709f21e65b5fedf2f1
Tezika Zhou <tezikazhou@gmail.com>
2018-05-04 09:03:13 +0000

    add the New page

--- RESTful_Routing/BlogSolution/app.js
+++ RESTful_Routing/BlogSolution/app.js
@@ -48,6 +48,22 @@ app.get("/blogs", function(req, res) {
     });
 });
 
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+app.post("/blogs", function(req, res) {
+    //create blog;
+    Blog.create(req.body.blog, function(err, blog) {
+        if (err) {
+            res.render("new");
+        }
+        else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The server for blog start successfully!");
 });
