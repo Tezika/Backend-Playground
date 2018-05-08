afa819205f4c6d34723efb25259513e555483ae6
Tezika Zhou <tezikazhou@gmail.com>
2018-05-08 08:05:27 +0000

    model the reference.js

--- Associations/reference.js
+++ Associations/reference.js
@@ -1,65 +1,36 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo_2");
 
-//POST - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
+var Post = require("./models/post");
+var User = require("./models/user");
+
+
+Post.create({
+    title: "How to cook the best burger pt.4",
+    content: "blah blah blah blah"
+}, function(err, post) {
+    if (err) {
+        console.log(post);
+    }
+    else {
+        User.findOne({ name: "Bob Blie" }, function(err, foundUsr) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                foundUsr.posts.push(post);
+                foundUsr.save(function(err, usr) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log(usr);
+                    }
+                });
+            }
+        });
+    }
 });
-
-var Post = mongoose.model("Post", postSchema);
-
-//USER - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    //Assoicate the post by referencing.
-    posts: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
-    }]
-});
-
-var User = mongoose.model("User", userSchema);
-
-// User.create({
-//     email: "bob.blie@gmail.com",
-//     name: "Bob Blie"
-// }, function(err, usr) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     else {
-//         console.log(usr);
-//     }
-// });
-
-// Post.create({
-//     title: "How to cook the best burger pt.3",
-//     content: "blah blah blah blah"
-// }, function(err, post) {
-//     if (err) {
-//         console.log(post);
-//     }
-//     else {
-//         User.findOne({ name: "Bob Blie" }, function(err, foundUsr) {
-//             if (err) {
-//                 console.log(err);
-//             }
-//             else {
-//                 foundUsr.posts.push(post);
-//                 foundUsr.save(function(err, usr) {
-//                     if (err) {
-//                         console.log(err);
-//                     }
-//                     else {
-//                         console.log(usr);
-//                     }
-//                 });
-//             }
-//         });
-//     }
-// });
 
 // Find user && Find all posts for that user had
 User.findOne({ name: "Bob Blie" }).populate("posts").exec(function(err, user) {
