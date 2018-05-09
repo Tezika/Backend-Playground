b110008eef3b3ccb16942bd1c96146ebd461fd4c
Tezika Zhou <tezikazhou@gmail.com>
2018-05-09 09:44:30 +0000

    add the authentication for registering

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -4,10 +4,28 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     LocalStrategy = require("passport-local"),
     passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user"),
     app = express();
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+    secret: "Tezika is most clever man over the world :)",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//=========
+//ROUTES
+//=========
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -15,6 +33,25 @@ app.get("/", function(req, res) {
 
 app.get("/secret", function(req, res) {
     res.render("secret");
+});
+
+//AUTH ROUTES
+//show sign up form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+//handling user sign up
+app.post("/register",function(req,res){
+   User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+      if (err) {
+          console.log(err);
+          return res.render('register');
+      }
+      passport.authenticate("local")(req, res, function(){
+         res.redirect("/secret"); 
+      });
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
