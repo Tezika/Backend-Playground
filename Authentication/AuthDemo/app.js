367689b1add313e64267545499228b9e2b1716db
Tezika Zhou <tezikazhou@gmail.com>
2018-05-09 10:20:08 +0000

    add the authentication for login

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -20,13 +20,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //=========
 //ROUTES
 //=========
-
 app.get("/", function(req, res) {
     res.render("home");
 });
@@ -42,17 +42,30 @@ app.get("/register", function(req, res) {
 });
 
 //handling user sign up
-app.post("/register",function(req,res){
-   User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-      if (err) {
-          console.log(err);
-          return res.render('register');
-      }
-      passport.authenticate("local")(req, res, function(){
-         res.redirect("/secret"); 
-      });
-   });
+app.post("/register", function(req, res) {
+    User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/secret");
+        });
+    });
 });
+
+// LOGIN ROUTES
+// show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+// handling user login
+// middleware(passport)
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res) {});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The server stared......");
