a20f40d96344add3526b705fa325297f9f47f17f
Tezika Zhou <tezikazhou@gmail.com>
2018-05-11 07:55:24 +0000

    add the comments' editing

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -59,6 +59,40 @@ router.post("/", isLoggedIn, function(req, res) {
     });
 });
 
+// EDIT
+router.get("/:comment_id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+            res.redirect("back");
+        }
+        else {
+            Comment.findById(req.params.comment_id, function(err, foundComment) {
+                if (err) {
+                    console.log(err);
+                    res.redirect("back");
+                }
+                else {
+                    res.render("comments/edit", { comment: foundComment, campground: foundCampground })
+                }
+            });
+        }
+    });
+});
+
+// UPDATE
+router.put("/:comment_id", function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+        else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
