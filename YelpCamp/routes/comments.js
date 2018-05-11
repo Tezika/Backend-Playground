63caca850cceccaad29207defc4ff4ff28f48aa3
Tezika Zhou <tezikazhou@gmail.com>
2018-05-11 09:25:54 +0000

    add the comment authorization.

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -60,32 +60,37 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 // EDIT
-router.get("/:comment_id/edit", function(req, res) {
-    Campground.findById(req.params.id, function(err, foundCampground) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
         if (err) {
             console.log(err);
             res.redirect("back");
         }
         else {
-            Comment.findById(req.params.comment_id, function(err, foundComment) {
-                if (err) {
-                    console.log(err);
-                    res.redirect("back");
-                }
-                else {
-                    res.render("comments/edit", { comment: foundComment, campground: foundCampground })
-                }
-            });
+            res.render("comments/edit", { comment: foundComment, campground_id: req.params.id })
         }
     });
 });
 
 // UPDATE
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
         if (err) {
             console.log(err);
+            res.redirect("back");
+        }
+        else {
             res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// DELETE
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err, removedComment) {
+        if (err) {
+            console.log(err);
+            res.redirect("back");
         }
         else {
             res.redirect("/campgrounds/" + req.params.id);
@@ -99,6 +104,27 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            }
+            else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                }
+                else {
+                    res.redirect("back");
+                }
+            }
+        });
+    }
+    else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
